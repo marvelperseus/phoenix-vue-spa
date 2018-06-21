9c1f99f57aac24641b0017d2e7a3931bab9fedff
serkandyck <serkandayicik@gmail.com>
2018-06-21 18:12:28 +0300

    setting webpack 4 and scss loaders

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,28 +1,70 @@
 const path = require('path');
-module.exports = function(env) {
-  const production = process.env.NODE_ENV === 'production';
-  return {
-    devtool: production ? 'source-maps' : 'eval',
-    entry: './js/app.js',
-    output: {
-      path: path.resolve(__dirname, '../priv/static/js'),
-      filename: 'app.js',
-      publicPath: '/',
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-          },
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const production = process.env.NODE_ENV === 'production';
+
+module.exports = (env, options) => ({
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({ cache: true, parallel: true, sourceMap: false })
+    ]
+  },
+  devtool: production ? 'source-maps' : 'eval',
+  entry: ['./js/app.js', './sass/app.scss'],
+  output: {
+    path: path.resolve(__dirname, '../priv/static'),
+    filename: 'js/app.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
         },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+                minimize: {
+                    safe: true
+                }
+            }
+          },
+          {
+            loader: "sass-loader",
+            options: {}
+          }
+        ]
+      }
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(
+      [
+        '*.*',
+        'css/*.*',
+        'images/*.*',
+        'js/*.*'
       ],
-    },
-    resolve: {
-      modules: ['node_modules', path.resolve(__dirname, 'js')],
-      extensions: ['.js'],
-    },
-  };
-};
+      {
+        root: path.resolve(__dirname, '../priv/static'),
+      }
+    ),
+    new MiniCssExtractPlugin({
+      path: path.resolve(__dirname, '../priv/static'),
+      filename: 'css/app.css',
+      publicPath: '/',
+    }),
+    new CopyWebpackPlugin([{ from: 'static/', to: path.resolve(__dirname, '../priv/static') }])
+  ]
+});
